78ac2a058dfc97213a2c95a4c7ed85b8a9364a9a
Emre <157912176+Emre03477@users.noreply.github.com>
2024-02-01 15:13:34 +0300

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -1,5 +1,4 @@
 module.exports = {
   name:"ip",
-  code: atob("JGFkZEZpZWxkW3BvcnQ7JGdldFZhcltwb3J0XTt0cnVlXQokYWRkRmllbGRbaXA7JGdldFZhcltp
-cF07dHJ1ZV0=")
+  code: atob("JGFkZEZpZWxkW3BvcnQ7JGdldFZhcltwb3J0O21haW5dO3RydWVdCiRhZGRGaWVsZFtpcDskZ2V0VmFyW2lwO21haW5dO3RydWVd")
              }
